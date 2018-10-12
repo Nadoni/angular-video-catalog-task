c5947e7c38d7c4fd2801d7a40b0cd6f9e27fce1c
Nadoni <kojaniydracon@gmail.com>
2018-10-12 10:51:15 +0300

    move course.model and temp-user.model, update courses.module

--- src/app/courses/components/interface/user.model.ts
+++
@@ -1,5 +0,0 @@
-export interface IUserModel {
-  id: number;
-  firstName: string;
-  lastName: string;
-}
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,18 +3,28 @@ import { CommonModule } from '@angular/common';
 import { CourseListComponent } from './components/course-list/course-list.component';
 import { CourseItemComponent } from './components/course-item/course-item.component';
 import {CoursesComponent} from './courses.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from '../material.module';
 import {SharedModule} from '../shared/shared.module';
+import { CourseAddComponent } from './components/course-add/course-add.component';
+import {CourseDetailsComponent} from './components/course-details/course-details.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule,
+    // temp way
   ],
-  declarations: [CoursesComponent, CourseListComponent, CourseItemComponent],
-  exports: [CoursesComponent]
+  declarations: [
+    CoursesComponent,
+    CourseListComponent,
+    CourseItemComponent,
+    CourseAddComponent,
+    CourseDetailsComponent
+  ],
+  exports: [CoursesComponent, CourseAddComponent]
 })
 export class CoursesModule { }
