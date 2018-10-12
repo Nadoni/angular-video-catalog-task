488f504c2b164d1adb5fdaf916092f4e3eaf4948
Anton_Lyashkevich <kojaniydracon@gmail.com>
2018-10-12 18:08:58 +0300

    add user.model, validation-constants, pages-routing and app-routing module, add _button.scss

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ContentComponent, LoginComponent, NotFoundPageComponent} from './components';
+import {SharedModule} from '../shared/shared.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../material.module';
+import {CoursesModule} from '../courses/courses.module';
+import {PagesRoutingModule} from '../pages/pages-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    CoursesModule,
+    PagesRoutingModule
+  ],
+  declarations: [LoginComponent, ContentComponent, NotFoundPageComponent],
+  exports: [LoginComponent, ContentComponent, NotFoundPageComponent]
+})
+export class PagesModule { }
