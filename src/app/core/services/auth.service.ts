883e889f87a3cd8dfd8ec7cd292addfef3c7e1e0
Nadoni <kojaniydracon@gmail.com>
2018-10-12 10:39:37 +0300

    feat: add mock up for auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +6,30 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   private storageKey = 'UserToken';
-  constructor() { }
+  public isLoggedUser = false;
 
-  login() {
-    localStorage.setItem(this.storageKey, 'token');
+  private existedUser = {email: 'admin@g.com', password: '123'};
+
+  constructor() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.existedUser));
+  }
+
+  login(user) {
+    return this.isLoggedIn(user);
+    // localStorage.setItem(this.storageKey, JSON.stringify(user));
+  }
+
+  logOut() {
+    localStorage.removeItem(this.storageKey);
+    this.isLoggedUser = false;
+  }
+
+  getUser() {
+    return JSON.parse(localStorage.getItem(this.storageKey));
+  }
+
+  isLoggedIn(user) {
+    const data = this.getUser();
+    return data.email === user.email ? (this.isLoggedUser = true) && true : false;
   }
 }
